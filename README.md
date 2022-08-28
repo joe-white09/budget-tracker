a59489c66478d5394f2bd3b6415cea29c0c85286
joe-white09 <joe_white09@yahoo.com>
2022-08-28 16:40:06 -0500

    updated readme and service worker

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Budget Tracker Starter Code
+# Budget Tracker
+
+## Purpose
+This is an application that allows you to keep up with your budget by entering your deposits and expenses
+
+## Built With 
+* Node
+* Express
+* Mongoose
+
+## Usage
+This application is a PWA (Progressive Web App) that allows users to keep their budget balance either online or offline
+
+## Contribution
+* Starter Code by Vanderbilt University Coding Bootcamp
+* Offline and PWA by Joe White
+
--- public/service-worker.js
+++ public/service-worker.js
@@ -3,12 +3,22 @@ const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION
 
 const FILES_TO_CACHE = [
+    "/",
     "/index.html",
     "/manifest.json",
-    "/public/css/styles.css",
-    "/public/js/index.js"
+    "/css/styles.css",
+    "/js/index.js",
+    "/js/idb.js",
+    "/icons/icon-72x72.png",
+    "/icons/icon-96x96.png",
+    "/icons/icon-128x128.png",
+    "/icons/icon-144x144.png",
+    "/icons/icon-152x152.png",
+    "/icons/icon-192x192.png",
+    "/icons/icon-384x384.png",
+    "/icons/icon-512x512.png"
 ];
-
+  
 // Install the service worker
 self.addEventListener('install', function(evt) {
     evt.waitUntil(
@@ -20,21 +30,35 @@ self.addEventListener('install', function(evt) {
   
     self.skipWaiting();
 });
-
+  
 // Activate the service worker and remove old data from the cache
 self.addEventListener('activate', function(evt) {
     evt.waitUntil(
       caches.keys().then(keyList => {
         return Promise.all(
           keyList.map(key => {
-            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            if (key !== CACHE_NAME) {
               console.log('Removing old cache data', key);
               return caches.delete(key);
             }
           })
         );
-      })
+        })
     );
   
     self.clients.claim();
+});
+  
+// Respond with cached resources
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+      caches.match(e.request).then(function (request) {
+        if (request) { // if cache is available, respond with cache
+          return request
+        } else {       // if there are no cache, try fetching request
+          return fetch(e.request)
+        }
+      })
+    )
 });
