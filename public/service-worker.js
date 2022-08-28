56ea003e318a3be87c77be7e0569df1af4172894
joe-white09 <joe_white09@yahoo.com>
2022-08-28 13:33:39 -0500

    updated service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,62 +3,38 @@ const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION
 
 const FILES_TO_CACHE = [
-    "./index.html",
-    "./manifest.json",
-    "./public/css/styles.css",
-    "./public/icons/icon-72x72.png",
-    "./public/icons/icon-96x96.png",
-    "./public/icons/icon-128x128.png",
-    "./public/icons/icon-144x144.png",
-    "./public/icons/icon-152x152.png",
-    "./public/icons/icon-192x192.png",
-    "./public/icons/icon-384x384.png",
-    "./public/icons/icon-512x512.png",
-    "./public/js/index.js"
+    "/index.html",
+    "/manifest.json",
+    "/public/css/styles.css",
+    "/public/js/index.js"
 ];
 
-// Respond with cached resources
-self.addEventListener('fetch', function(e) {
-    console.log('fetch request : ' + e.request.url)
-    e.respondWith(
-        caches.match(e.request).then(function (request) {
-            if (request) { //if cache is available, respond with cache
-                console.log('responding with cache : ' + e.request.url)
-                return request
-            } else { // if there are no chace, try fetching request
-                console.log('file is not cached, fetching : ' + e.request.url)
-                return fetch(e.request)
-            }
-        })
-    )
-})
-
-// Cache resources
-self.addEventListener('install', function(e) {
-    e.waitUntil(
-        caches.open(CACHE_NAME).then(function (cache) {
-            console.log('installing cache : ' + CACHE_NAME)
-            return cache.addAll(FILES_TO_CACHE)
-        })
-    )
-})
-
-// Delete outdated caches
-self.addEventListener('activate', function (e) {
-    e.waitUntil(
-        caches.keys().then(function (keyList) {
-            let cacheKeepList = keyList.filter(function (key) {
-                return key.indexOf(APP_PREFIX);
-            })
-            // Add current cache name to keepList
-            cacheKeepList.push(CACHE_NAME);
-
-            return Promise.all(keyList.map(function (key, i) {
-                if (cacheKeepList.indexOf(key) === -1) {
-                    console.log('deleting cache : ' + keyList[i] );
-                    return caches.delete(keyList[i]);
-                }
-            }));
-        })
+// Install the service worker
+self.addEventListener('install', function(evt) {
+    evt.waitUntil(
+      caches.open(CACHE_NAME).then(cache => {
+        console.log('Your files were pre-cached successfully!');
+        return cache.addAll(FILES_TO_CACHE);
+      })
     );
+  
+    self.skipWaiting();
+});
+
+// Activate the service worker and remove old data from the cache
+self.addEventListener('activate', function(evt) {
+    evt.waitUntil(
+      caches.keys().then(keyList => {
+        return Promise.all(
+          keyList.map(key => {
+            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+              console.log('Removing old cache data', key);
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+    );
+  
+    self.clients.claim();
 });
